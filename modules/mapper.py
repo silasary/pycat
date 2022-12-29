ea31b466b73dc220f57dd94bfa7870eaf493ea9b
Katelyn Gigante <clockwork.singularity@gmail.com>
2022-12-30 10:56:42 +1100

    Reenable automatic map saving

--- modules/mapper.py
+++ modules/mapper.py
@@ -443,7 +443,7 @@ class Mapper(BaseModule):
         return outstr
 
     def quit(self, args=None):
-        # self.save([self.mapfname])
+        self.save([self.mapfname])
         if 'visited' not in self.world.state:
             self.world.state['visited'] = set()
         self.log("Visited {} rooms today!".format(len(self.world.state['visited'])))
@@ -604,7 +604,7 @@ class Mapper(BaseModule):
     def currentArea(self):
         return self.m.getRoomData(self.current())['zone']
 
-    def unmapped(self, unvisited, inArea, one):
+    def unmapped(self, unvisited: bool, inArea: bool, one: bool):
         if 'visited' not in self.world.state:
             self.world.state['visited'] = set()
         out = []  # A set would probably be smaller, but a list is in the order of closeness.
