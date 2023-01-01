7231d698a14122324896fd9120cac934eec643ef
Katelyn Gigante <clockwork.singularity@gmail.com>
2023-01-02 00:54:56 +1100

    Add some useful variables to the eval environment

--- modules/eval.py
+++ modules/eval.py
@@ -1,3 +1,4 @@
+from typing import Any
 from modules.basemodule import BaseModule
 from pprint import pformat
 
@@ -6,9 +7,19 @@ class Eval(BaseModule):
     def alias(self, line):
         if line.startswith('#py '):
             rest = line[4:]
-            self.mud.log("\n" + pformat(eval(rest)))
+            self.mud.log("\n" + pformat(eval(rest, self.globals())))
             return True
         elif line.startswith('#pye '):
             rest = line[5:]
             exec(rest)
             return True
+
+    def globals(self) -> dict[str, Any]:
+        return {
+            'self': self,
+            'mud': self.mud,
+            'world': self.mud.world,
+            'gmcp': self.mud.world.gmcp,
+            'state': self.mud.world.state,
+            'modules': self.mud.world.modules,
+        }
